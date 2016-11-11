7941601261a551cb3e11a1e1a044d249147e4261
Diego Romero <diegor.romero@yahoo.com>
2016-11-10 22:56:53 -0600

    minor fixing

--- src/main/java/com/cirro/pojos/T1.java
+++ src/main/java/com/cirro/pojos/T1.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.cirro.pojos;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 
